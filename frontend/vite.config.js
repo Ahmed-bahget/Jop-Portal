1559a7a1381745031b9d389bb7f4f19c82757d2b
Ahmed-bahget <ahedbahget70@gmail.com>
2025-04-08 13:43:57 +0200

    add base in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://absolute-naomi-ahmedbahget-b6d6f38f.koyeb.app',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
